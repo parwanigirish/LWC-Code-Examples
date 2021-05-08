2d0c60f461efed4e83c15eb2e069c4e90ebdd619
parwanigirish <parwani.girish@gmail.com>
2021-05-08 10:55:48 +0530

    Create LDSBasedBaseComponents.js

---
+++ LDSBasedBaseComponents.js
@@ -0,0 +1,11 @@
+import { LightningElement, track } from 'lwc';
+
+export default class AccountManagerLDSForms extends LightningElement {
+
+    @track recordId;
+  
+    successHandler(event){
+        this.recordId = event.detail.id;
+    }
+  
+}
